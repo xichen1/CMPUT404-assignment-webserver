016279531d2c3ee228577106b15c9fed4f12f3ec
xichen1 <xichen.pan@gmail.com>
2021-09-21 01:38:20 -0400

    finish server

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 #  coding: utf-8 
+from genericpath import isdir, isfile
 import socketserver
+from os import getcwd, path, stat_result
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -27,12 +30,81 @@ import socketserver
 # try: curl -v -X GET http://127.0.0.1:8080/
 
 
+error404 = '''
+        <!DOCTYPE html>
+        <html>
+            <body>404 Not Found</body>
+        </html>
+'''
+
 class MyWebServer(socketserver.BaseRequestHandler):
+    response = ""
+
+    #REF: https://security.openstack.org/guidelines/dg_using-file-paths.html
+    def is_safe_path(self, basedir, path):
+        # resolves symbolic links
+        matchpath = os.path.realpath(path)
+        return basedir == os.path.commonpath((basedir, matchpath))
+
+    def status404(self):
+        self.response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=utf-8\r\n\r\n" + error404 + "\r\n"
+        return
+
+    def handleFile(self, filePath):
+        _name, extension = os.path.splitext(filePath)
+        htmlFile = open(filePath)
+        if(extension == ".html"):
+            self.response = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n" + htmlFile.read() + "\r\n"
+        elif(extension == ".css"):
+            self.response = "HTTP/1.1 200 OK\r\nContent-Type: text/css; charset=utf-8\r\n\r\n" + htmlFile.read() + "\r\n"
+        htmlFile.close()
+        return
     
+    def handleDir(self, filePath, requestUrl):
+        if(requestUrl[-1] != "/"):
+            newLocation = "http://127.0.0.1:8080" + requestUrl + "/"
+            self.response = "HTTP/1.1 301 Moved Permanently\r\nLocation: " + newLocation + "\r\n"
+        else:
+            if(path.isfile(filePath+"index.html")):
+                htmlFile = open(filePath+"index.html")
+                self.response = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n" + htmlFile.read() + "\r\n"
+                htmlFile.close()
+            else:
+                self.status404()
+        return
+
+    def handleUrl(self, requestList):
+        requestUrl = requestList[0].split()[1]
+        filePath = "www" + requestUrl
+        if(self.is_safe_path(getcwd() + "/www", getcwd() + "/www" + requestUrl) == False):
+            self.status404()
+            return
+        if(path.isdir(filePath)):
+            self.handleDir(filePath, requestUrl)
+        elif(path.isfile(filePath)):
+            self.handleFile(filePath)
+        else:
+            self.status404()
+        return
+
+    def handleRequestType(self, requestList):
+        requestType = requestList[0].split()[0]
+        if (requestType != "GET"):
+            self.response = "HTTP/1.1 405 Method Not Allowed\r\n"
+            return
+        else:
+            self.handleUrl(requestList)
+            return
+
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        # print ("Got a request of: %s\n" % self.data)
+
+        requestList = self.data.decode("utf-8").split('\n')
+        self.handleRequestType(requestList)
+    
+        self.request.sendall(bytearray(self.response,'utf-8'))
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
